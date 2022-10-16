34bdd794b9442e32f96b7fe937f058808769c6e4
Parviz <parviz.hajili@gmail.com>
2022-10-16 20:23:34 +0400

    Create Entities

---
+++ Core/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Common;
+
+namespace Core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Core/Entities/ProductFeature.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Common;
+
+namespace Core.Entities
+{
+    public class ProductFeature : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Colour { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
